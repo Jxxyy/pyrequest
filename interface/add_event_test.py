8015086e60510b25ea5cbe333cf062339898b4e4
Jxxyy <smtliuguiqian@163.com>
2019-07-22 15:10:31 +0800

    增加http报告

--- interface/add_event_test.py
+++ interface/add_event_test.py
@@ -5,41 +5,42 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parentdir)   #添加优先路径
 from db_fixture import test_data
 class AddEventTest(unittest.TestCase):
-''' 添加发布会 '''
+#添加发布会
 	def setUp(self):
 		self.base_url = "http://127.0.0.1:8000/api/add_event/"
 	def tearDown(self):
 		print(self.result)
 	def test_add_event_all_null(self):
-''' 所有参数为空 '''
-		payload = {'eid':'','':'','limit':'','address':"",'start_time':''}
+#所有参数为空
+		payload = {'eid':'','name':'','limit':'','address':'','start_time':''}
 		r = requests.post(self.base_url, data=payload)
+		print (r.json())
 		self.result = r.json()
 		self.assertEqual(self.result['status'], 10021)
 		self.assertEqual(self.result['message'], 'parameter error')
 	def test_add_event_eid_exist(self):
-''' id 已经存在 '''
+#id 已经存在
 		payload = {'eid':1,'name':'一加 4 发布会','limit':2000,'address':"深圳宝体",'start_time':'2017'}
 		r = requests.post(self.base_url, data=payload)
 		self.result = r.json()
 		self.assertEqual(self.result['status'], 10022)
 		self.assertEqual(self.result['message'], 'event id already exists')
 	def test_add_event_name_exist(self):
-''' 名称已经存在 '''
-		payload = {'eid':11,'name':'红米 Pro 发布会','limit':2000'address':"北京水立方",'start_time':'2017'}
+#名称已经存在
+		payload = {'eid':11,'name':'红米 Pro 发布会','limit':2000,'address':'北京水立方','start_time':'2017'}
 		r = requests.post(self.base_url,data=payload)
 		self.result = r.json()
 		self.assertEqual(self.result['status'], 10023)
 		self.assertEqual(self.result['message'], 'event name already exists')
 	def test_add_event_data_type_error(self):
-''' 日期格式错误 '''
+#日期格式错误
 		payload = {'eid':11,'name':'一加 4 手机发布会','limit':2000,'address':"北京水立方",'start_time':'2017'}
 		r = requests.post(self.base_url,data=payload)
 		self.result = r.json()
 		self.assertEqual(self.result['status'], 10024)
 		self.assertIn('start_time format error.', self.result['message'])
 	def test_add_event_success(self):
-''' 添加成功 '''
+#添加成功
 		payload = {'eid':11,'name':'一加 4 手机发布会','limit':2000,'address':"北京水立方",'start_time':'2017-05-10 12:00:00'}
 		r = requests.post(self.base_url,data=payload)
 		self.result = r.json()
